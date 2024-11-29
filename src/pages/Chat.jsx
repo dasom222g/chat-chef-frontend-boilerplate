250e906446fa30a52924beb208325e1f887c0d9e
dasom222g <dasom228@gmail.com>
2024-11-29 14:29:31 +0900

    Add chat logic

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,28 +1,132 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import MessageBox from "../components/MessageBox";
 import PrevButton from "../components/PrevButton";
 import { MoonLoader } from "react-spinners";
 
 const Chat = ({ ingredientList }) => {
   // logic
-  console.log("ingredientList", ingredientList);
+  const ENDPOINT = process.env.REACT_APP_SERVER_ADDRESS;
 
   const [value, setValue] = useState("");
 
   // TODO: set함수 추가하기
-  const [messages] = useState([]); // chatGPT와 사용자의 대화 메시지 배열
-  const [isInfoLoading] = useState(false); // 최초 정보 요청시 로딩
-  const [isMessageLoading] = useState(true); // 사용자와 메시지 주고 받을때 로딩
-  const hadleChange = (event) => {
+  const [messages, setMessages] = useState([]); // chatGPT와 사용자의 대화 메시지 배열
+  const [isInfoLoading, setIsInfoLoading] = useState(true); // 최초 정보 요청시 로딩
+  const [isMessageLoading, setIsMessageLoading] = useState(false); // 사용자와 메시지 주고 받을때 로딩
+
+  const [infoMessages, setInfoMessages] = useState([]); // 초기 세팅 메시지
+
+  const handleChange = (event) => {
     const { value } = event.target;
-    console.log("value==>", value);
     setValue(value);
   };
 
-  const hadleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    const userMessage = {
+      role: "user",
+      content: value.trim(),
+    };
+
+    setMessages((prev) => [...prev, userMessage]);
+
+    // TODO: api 호출
+    sendMessage(userMessage);
+
+    setValue("");
+
     console.log("메시지 보내기");
   };
+
+  // 1. 컴포넌트 내에 있는 state값 변경시 호출
+  // useEffect(() => {});
+
+  // 2. 해당 컴포넌트 생성시 호출(딱 한번만 실행)
+  // useEffect(() => {}, []);
+
+  // 3. 지정한 state값 변경시 실행
+  // useEffect(() => {}, [value, messages]);
+
+  // 최초 정보 세팅
+  const sendInfo = useCallback(async () => {
+    // 로딩 on
+    setIsInfoLoading(true);
+    console.log("env!!", process.env.REACT_APP_SERVER_ADDRESS);
+    try {
+      const response = await fetch(`${ENDPOINT}/recipe`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ ingredientList }),
+      });
+
+      const result = await response.json();
+      console.log("🚀 result:", result);
+
+      // 최초 세팅 메시지 저장
+      const removeLastDataList = result.data.filter(
+        (item) => item.role !== "assistant"
+      );
+
+      setInfoMessages(removeLastDataList);
+
+      // messages의 배열을 변경
+      const lastItem = result.data.find((item) => item.role === "assistant");
+      console.log("🚀 ~ sendInfo ~ lastItem:", lastItem);
+
+      const { role, content } = lastItem;
+
+      setMessages((prev) => [
+        ...prev,
+        {
+          role,
+          content,
+        },
+      ]);
+
+      setIsInfoLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [ENDPOINT, ingredientList]);
+
+  const sendMessage = useCallback(
+    async (userMessage) => {
+      // 로딩 on
+      setIsMessageLoading(true);
+      try {
+        const response = await fetch(`${ENDPOINT}/message`, {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({
+            userMessage,
+            messages: [...infoMessages, ...messages],
+          }),
+        });
+
+        const result = await response.json();
+        console.log("🚀 result:", result);
+
+        const { role, content } = result.data;
+
+        setMessages((prev) => [...prev, { role, content }]);
+
+        setIsMessageLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [ENDPOINT, infoMessages, messages]
+  );
+
+  useEffect(() => {
+    // 최초 진입시 실행
+    ingredientList.length && sendInfo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sendInfo]);
+
+  useEffect(() => {
+    console.log("🚀 ~ Chat ~ messages:", messages);
+  }, [messages]);
 
   // view
   return (
@@ -57,14 +161,14 @@ const Chat = ({ ingredientList }) => {
           <form
             id="sendForm"
             className="w-full px-2 h-full"
-            onSubmit={hadleSubmit}
+            onSubmit={handleSubmit}
           >
             <input
               className="w-full text-sm px-3 py-2 h-full block rounded-xl bg-gray-100 focus:"
               type="text"
               name="message"
               value={value}
-              onChange={hadleChange}
+              onChange={handleChange}
             />
           </form>
           <button
